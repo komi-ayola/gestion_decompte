fabfac95c3a47a8266a54dac320dd499d5311689
komiayola <komi.ayola@outlook.fr>
2025-02-20 16:03:30 +0000

    Suppression des fichiers ignorés

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'ma_cle_secrete')
-    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:komi0101@localhost/employe')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///local.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
